8c37a65f5d0ca1dea6149f5889aee1dcbb6d19a3
Martin Šimeček <simecek.m@seznam.cz>
2021-06-08 22:16:50 +0200

    add UI for ThemeSwitcher component

--- src/component/App.js
+++ src/component/App.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Content from "component/Content";
 import Columns from "component/Columns";
 import Logo from "component/Logo";
+import ThemeSwitcher from "component/ThemeSwitcher";
 
 const Title = styled.h1`
   font-size: 92px;
@@ -22,9 +23,16 @@ const Text = styled.div`
   font-size: 36px;
 `;
 
+const TopThemeSwitcher = styled(ThemeSwitcher)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
 function App() {
   return (
     <Content>
+      <TopThemeSwitcher />
       <Columns>
         <Logo />
         <div>
---
+++ src/component/ThemeSwitcher.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import styled from "styled-components";
+import SvgIcon from "./SvgIcon";
+
+import { ReactComponent as Night } from "assets/night.svg";
+import { ReactComponent as Day } from "assets/day.svg";
+
+const Knob = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 25em;
+  transition: .5s ease all;
+  position: absolute;
+  top: 10px;
+`;
+
+const NightIcon = styled(SvgIcon)`
+  position: absolute;
+  left: 10px;
+  width: 30px;
+  top: 10px;
+  fill: white;
+  transition: .3s ease all;
+`;
+
+const DayIcon = styled(SvgIcon)`
+  position: absolute;
+  right: 10px;
+  width: 30px;
+  top: 10px;
+  transition: .3s ease all;
+  fill: black;
+`;
+
+const Wrapper = styled.div`
+  width: 90px;
+  height: 50px;
+  background-color: ${props => props.isDarkTheme ? "black" : "#F5F5F5"};
+  border-radius: 25em;
+  cursor: pointer;
+  position: relative;
+  transition: 1s ease all;
+
+  ${Knob} {
+    z-index: 10;
+    left: ${props => props.isDarkTheme ? "50px" : "10px"};
+    background-color: ${ props => props.isDarkTheme ? "white" : "#08ED7B"}
+  }
+
+  ${DayIcon} {
+    opacity: ${props => props.isDarkTheme ? "0" : "100"};
+  }
+
+  ${NightIcon} {
+    opacity: ${props => props.isDarkTheme ? "100" : "0"};
+  }
+`;
+
+
+
+function ThemeSwitcher({ className }) {
+  const [isDarkTheme, setDarkTheme] = useState(false);
+  const switchTheme = () => setDarkTheme(!isDarkTheme)
+  return(
+    <Wrapper className={className} isDarkTheme={isDarkTheme} onClick={switchTheme}>
+      <NightIcon Icon={Night} />
+      <Knob />
+      <DayIcon Icon={Day} />
+    </Wrapper>
+  )
+};
+
+export default ThemeSwitcher;
